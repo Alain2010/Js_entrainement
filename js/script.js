c79b021109c55a3d8aaec7e6379c835cceb1cdda
Alain2010 <alainmvumbi19@gmail.com>
2023-10-30 01:08:25 +0100

    mise à jour

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
-// const chrono = document.querySelector('h1');
+const chrono = document.querySelector('h1');
 
-// function compteur () {
+function compteur () {
     
-// const dateDepart = new Date().getTime();
-// console.log(dateDepart);
+const dateDepart = new Date().getTime();
+console.log(dateDepart);
 
-// const dateFinal = new Date('Novembre 1, 2023').getTime();
-// console.log(dateFinal);
+const dateFinal = new Date('Novembre 1, 2023').getTime();
+console.log(dateFinal);
 
-// const difference = dateFinal - dateDepart
-// console.log(difference);
+const difference = dateFinal - dateDepart
+console.log(difference);
 
-// const jour = Math.floor(difference / (1000 * 60 * 60 * 24));
-// console.log(jour);
-// const heures = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+const jour = Math.floor(difference / (1000 * 60 * 60 * 24));
+console.log(jour);
+const heures = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
 
-// console.log(heures);
+console.log(heures);
 
-// const minutes = Math.floor((difference % (1000 * 60 * 60 )) / (1000 * 60 ))
-// console.log(minutes);
+const minutes = Math.floor((difference % (1000 * 60 * 60 )) / (1000 * 60 ))
+console.log(minutes);
 
-// const secondes = Math.floor((difference % (1000 * 60 )) / 1000)
-// console.log(secondes);
+const secondes = Math.floor((difference % (1000 * 60 )) / 1000)
+console.log(secondes);
 
-// chrono.innerHTML = `Il vous reste ${jour}j ${heures}h ${minutes}mn ${secondes}s pour profiter de l'essai gratuit`
-// }
-// compteur();
-// const activation = setInterval(function () {
-//     return compteur();
-// }, 1000);
+chrono.innerHTML = `Il vous reste ${jour}j ${heures}h ${minutes}mn ${secondes}s pour profiter de l'essai gratuit`
+}
+compteur();
+const activation = setInterval(function () {
+    return compteur();
+}, 1000);
 
-// const motDePasse1 = document.getElementById('password').value;
-// const motDePasse2 = document.getElementById('password2').value;
-// console.log(motDePasse1, motDePasse2);
-// console.log("hello");
+const motDePasse1 = document.getElementById('password').value;
+const motDePasse2 = document.getElementById('password2').value;
+console.log(motDePasse1, motDePasse2);
+console.log("hello");
 
 
 
