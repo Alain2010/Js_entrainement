7b8dccfe1815be0c5ee6a8b7cd96cdc356f578a9
Alain2010 <alainmvumbi19@gmail.com>
2023-10-30 01:04:48 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// const chrono = document.querySelector('h1');
+
+// function compteur () {
+    
+// const dateDepart = new Date().getTime();
+// console.log(dateDepart);
+
+// const dateFinal = new Date('Novembre 1, 2023').getTime();
+// console.log(dateFinal);
+
+// const difference = dateFinal - dateDepart
+// console.log(difference);
+
+// const jour = Math.floor(difference / (1000 * 60 * 60 * 24));
+// console.log(jour);
+// const heures = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+
+// console.log(heures);
+
+// const minutes = Math.floor((difference % (1000 * 60 * 60 )) / (1000 * 60 ))
+// console.log(minutes);
+
+// const secondes = Math.floor((difference % (1000 * 60 )) / 1000)
+// console.log(secondes);
+
+// chrono.innerHTML = `Il vous reste ${jour}j ${heures}h ${minutes}mn ${secondes}s pour profiter de l'essai gratuit`
+// }
+// compteur();
+// const activation = setInterval(function () {
+//     return compteur();
+// }, 1000);
+
+// const motDePasse1 = document.getElementById('password').value;
+// const motDePasse2 = document.getElementById('password2').value;
+// console.log(motDePasse1, motDePasse2);
+// console.log("hello");
+
+
+
+
+
+
+
+
